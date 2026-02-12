9954bb796b7af63ff36a93ac6f8a0d4ce274cc43
e-j-l <emmettjleary@gmail.com>
2026-02-11 20:37:29 -0500

    additions: Added multiplication and division operators, added error statement for unexpected inputs

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,9 @@
+//======================================
 // Calculator Class 
 // Emmett Leary
 // Assignment 3 
 // February 10, 2026
+//======================================
 
 import java.util.Scanner;
 
@@ -10,30 +12,50 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
 
         // asks and stores first number
-        System.out.print("Enter the first whole number: ");
+        System.out.print("Enter the first whole number: \n~ ");
         int num1 = scanner.nextInt();
 
         // asks and stores second number
-        System.out.print("Enter the second whole number: ");
+        System.out.print("Enter the second whole number: \n~ ");
         int num2 = scanner.nextInt();
 
-        // Ask the user to choose addition or subtraction 
-        System.out.print("Type 'add' for addition or 'sub' for subtraction: ");
-        String addsub = scanner.next();
+        // Ask the user to choose addition, subtraction, multiplication and division 
+        System.out.print("Type one of the following: \n" +
+                         "-------------------------- \n"+
+                         "'add' for addition \n"+
+                         "'sub' for subtraction \n"+
+                         "'multi' for multiplication \n"+
+                         "'div' for division \n"+
+                         "-------------------------- \n~ ");
+        String choice = scanner.next();
 
         // creates a place to store the result
         int result;
 
         // if-else to decide operation
-        if(addsub.equals("add")){
+        if(choice.equals("add")){
             // addition operation
             result = num1 + num2;
             System.out.println("Result: " + result);
 
-        }   else{
+        }   else if(choice.equals("sub")){
             // subtraction operation
             result = num1 - num2;
             System.out.println("Result: " + result);
+
+        }   else if(choice.equals("multi")){
+            // multiplication operation
+            result = num1 * num2;
+            System.out.println("Result: " + result);
+
+        }   else if(choice.equals("div")){
+            // division operation
+            result = num1 / num2;
+            System.out.println("Result: " + result);
+
+        }   else{
+            // added line for unexpected user inputs
+            System.out.println("Error: not an operation option");
 
         }
 
